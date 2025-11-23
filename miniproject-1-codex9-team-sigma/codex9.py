ffd4fe49d1ad1439952562f99fb8c2a70805c122
Radoslav <Radoslav@DESKTOP-OC22MTD>
2025-11-23 18:17:12 +0200

    Completed Task 2

--- miniproject-1-codex9-team-sigma/codex9.py
+++ miniproject-1-codex9-team-sigma/codex9.py
@@ -80,7 +80,8 @@ def organize_segments(words: list, module_numbers: list) -> list:
         [('code', 104), ('python', 215), ('Data', 309), ('structure', 412), ('Algorithms', 518)]
     """
     # TODO: Combine words and IDs into tuples
-    pass
+    combined = list(zip(words, module_numbers))
+    return combined
 
 
 # =======================================================
